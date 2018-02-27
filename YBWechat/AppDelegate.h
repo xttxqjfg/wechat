fb2615881849d9a84b99e1a86179eaffa214bff6
yb <yibo3513@hotmail.com>
2018-02-27 14:24:05 +0800

    no message

---
+++ YBWechat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YBWechat
+//
+//  Created by 易博 on 2018/1/24.
+//  Copyright © 2018年 易博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
